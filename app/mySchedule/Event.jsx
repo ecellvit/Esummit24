a3a2c65860620ff9ace7bf9629c3e04fcd9cc9d4
Arjun Bector <arjunbector@gmail.com>
2024-04-15 21:51:56 +0530

    Update Event.jsx

--- app/mySchedule/Event.jsx
+++ app/mySchedule/Event.jsx
@@ -1,65 +1,117 @@
-import image from "@/public/assets/boardpics/image.png";
-const scheduleDetails = [
-  {
-    id: 1,
-    eventName: "Innoventure",
-    date: "April 13",
-    description:
-      "Innoventure is a business simulation event that offers a platform to the participants to showcase their problem-solving skills. Covering aspects from product development to business analysis, Innoventure provides hands-on experience with the startup lifecycle.",
-    image: image,
-    venue: "Bhagat Singh Gallery ,SJT",
-    time: "10:00 AM to 07:00 PM",
-    whatsapp: "https://chat.whatsapp.com/E9iJqPqJbMHBUCn2GfCf1N",
-    loc: "https://maps.app.goo.gl/zmn9e7PeoWbDQ5gd6",
-  },
-  {
-    id: 3,
-    eventName: "Financial literacy Workshop",
-    date: "April 14",
-    description:
-      "Financial Literacy Workshop is a workshop where participants will explore fundamental aspects of personal finance. They will investigate essential topics such as budgeting, saving methods, and diverse investment strategies. Through this workshop, participants will gain practical knowledge and skills to manage finances effectively, paving the way for long-term financial security.",
-    image: image,
-    venue: "CHANNA REDDY AUDITORIUM, MB",
-    time: "10:00 AM to 12:00 PM",
-    whatsapp: "",
-    loc: "https://maps.app.goo.gl/ksZpHxsiXcZ6zjD47",
-  },
-  {
-    id: 4,
-    eventName: "Achievers' Conclave",
-    date: "April 14",
-    description:
-      "Achiever's Conclave is an event featuring speaker sessions by accomplished Achievers from diverse backgrounds. Participants will have the opportunity to gain invaluable insights and motivation as these achievers share their inspirational journeys and secrets to success, empowering them to reach new heights in all their endeavors.",
-    image: image,
-    venue: "CHANNA REDDY AUDITORIUM, MB",
-    time: "02:00 PM to 07:00 PM",
-    whatsapp: "",
-    loc: "https://maps.app.goo.gl/ksZpHxsiXcZ6zjD47",
-  },
-  {
-    id: 5,
-    eventName: "E-TALK",
-    date: "April 15",
-    description:
-      "E-Talk is a pragmatic session that will feature prominent personalities from the startup world to give the audience a deeper insight into the vast world of entrepreneurship. It is a platform for all the business aspirants out there to learn from the best and witness a sensational marathon of motivational speeches.",
-    image: image,
-    venue: "ANNA AUDITORIUM",
-    time: "02:00 PM to 07:00 PM",
-    whatsapp: "",
-    loc: "https://maps.app.goo.gl/N9De5HkUC7Gcgqga8",
-  },
-  {
-    id: 2,
-    eventName: "Ideathon",
-    date: "April 16",
-    description:
-      "The Ideathon is an event based on the participants' entrepreneurial spirit where they will brainstorm and pitch innovative business ideas through teamwork and creativity and develop actionable plans to tackle real-world challenges, competing for recognition.",
-    image: image,
-    venue: "Sarojini Naidu Gallery",
-    time: "10:00 AM to 05:00 PM",
-    whatsapp: "https://chat.whatsapp.com/EIxnuFBAJheHUTcGm5B0zr",
-    loc: "https://maps.app.goo.gl/9hPWa6ir3g7UQcSZ6",
-  },
-];
+import Loader from "@/components/Loader";
+import { useSession } from "next-auth/react";
+import { useEffect, useState } from "react";
+import { FaLocationDot } from "react-icons/fa6";
+import { GrMapLocation } from "react-icons/gr";
+import {
+  FaRegClock,
+  FaInfoCircle,
+  FaCalendarAlt,
+  FaWhatsapp,
+} from "react-icons/fa";
+import ScheduleRegisterButton from "@/components/events/ScheduleRegisterButton";
+import Link from "next/link";
 
-export default scheduleDetails;
+const Event = ({ event, userDetails }) => {
+  const [loader, setLoader] = useState(false);
+  const [showWarning, setShowWarning] = useState(false);
+  const { data: session, status } = useSession();
+  useEffect(() => {
+    if (event.id == 1) {
+      if (!userDetails.user.event1TeamId) {
+        setShowWarning(true);
+      }
+    }
+    if (event.id == 2) {
+      if (!userDetails.user.event2TeamId) {
+        setShowWarning(true);
+      }
+    }
+  }, []);
+  return (
+    <div>
+      <div>
+        {loader && <Loader />}
+        <div className="">
+          <h1 className="uppercase text-2xl md:text-3xl lg:text-5xl font-bold bg-gradient-to-br from-[#DCA64E] via-[#FEFAB7] to-[#D6993F] bg-clip-text text-transparent">
+            {event.eventName}
+          </h1>
+          <div className="uppercase flex gap-2 items-center">
+            <span>
+              <FaCalendarAlt />
+            </span>
+            {event.date}
+          </div>
+          <div className="uppercase flex gap-2 items-center">
+            <span>
+              <FaRegClock />
+            </span>
+            {event.time}
+          </div>
+          <div className="uppercase flex gap-2 items-center">
+            <span>
+              <FaLocationDot />
+            </span>
+            {event.venue}
+          </div>
+        </div>
+        <p className="font-poppins py-2">{event.description}</p>
+        <div className="flex flex-col md:flex-row gap-2">
+          <ScheduleRegisterButton
+            loader={loader}
+            setLoader={setLoader}
+            event={event.id}
+            token={session?.accessTokenBackend}
+          />
+          {(event.id === 1 || event.id === 2) && (
+            <button
+              className="text-black font-semibold hover:scale-105 transition-all bg-gradient-to-br from-[#DCA64E] via-[#FEFAB7] to-[#D6993F] p-2 rounded-lg hover:bg-opacity-80"
+              onClick={() => {
+                window.location.href = `/events/event${event.id}/memberDash`;
+              }}
+            >
+              Go to Dashboard
+            </button>
+          )}
+          {event.loc && (
+            <Link
+              className="text-black flex justify-center items-center gap-2 capitalize font-semibold hover:scale-105 transition-all bg-gradient-to-br from-[#DCA64E] via-[#FEFAB7] to-[#D6993F] p-2 rounded-lg hover:bg-opacity-80"
+              target="_blank"
+              href={event.loc}
+            >
+              <span>
+                <GrMapLocation />
+              </span>
+              get directions
+            </Link>
+          )}
+          {(event.id === 1 || event.id === 2) && (
+            <button className="bg-[#50CC5E] p-2 rounded-xl font-semibold">
+              <Link
+                className="flex items-center gap-2 justify-center"
+                href={event.whatsapp}
+              >
+                <span className="h-full">
+                  <FaWhatsapp className="p-0 m-0 text-xl" />
+                </span>
+                Join whatsapp group
+              </Link>
+            </button>
+          )}
+        </div>
+        {showWarning && (
+          <p className="bg-green-500 bg-opacity-100 flex items-center rounded-lg gap-2 p-2 my-2 w-fit">
+            <span>
+              <FaInfoCircle />
+            </span>
+            Congratulations on registering for the event! To participate, you
+            must create your team by April 11 2024 consisting of 3-4 members
+            only. You can create your team by going to dashboard.
+          </p>
+        )}
+      </div>
+    </div>
+  );
+};
+
+export default Event;
